2e568973df1943d5aec10164cd4ffbace7848b52
Muthukumar Arumugam <amkumar712@gmail.com>
2018-06-14 22:07:10 +0530

    Add files via upload

--- src/main/java/com/studiohub/login/application/controller/LoginController.java
+++ src/main/java/com/studiohub/login/application/controller/LoginController.java
@@ -4,38 +4,32 @@
 package com.studiohub.login.application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.studiohub.login.application.manager.IUserManager;
 import com.studiohub.login.application.manager.UserManager;
-import com.studiohub.login.application.model.User;
-import com.studiohub.login.application.model.UserAuthenticate;
-import com.studiohub.login.application.response.UserInfo;
-import com.studiohub.login.application.service.CustomUserDetailService;
 
 /**
  * @author Muthu
  *
  */
 
-@RestController
+@Controller
 public class LoginController {
 	
 	@Autowired
 	UserManager userManager;
 	
-	@PostMapping("/login")
-	public UserInfo login() {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
 		
-		UserInfo userInfo = userManager.getUserInfo("amkumar712");
-		
-		System.out.println(userInfo.getUserId());
-		System.out.println(userInfo.getEmail());
-		
-		return userInfo;
+		return "user/login";
 	}
 	
 	@PostMapping("/authenticate")
